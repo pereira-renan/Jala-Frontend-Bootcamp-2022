3b5863bab9c37955377be635ee68efd964937a61
Renan Pereira <renan.desouza@jala-foundation.org>
2022-04-25 17:08:46 -0300

    useEffect calling api, passing as props to a component that loops and renders data

---
+++ shop/src/components/BookCard.js
@@ -0,0 +1,35 @@
+import React from "react"
+import "./BookCard.css"
+
+export default function BookCard({ book }) {
+  return (
+    <>
+      <div className="bookCard__container">
+        <div className="bookCard_thumbnail__container">
+          <img
+            className="bookCard_thumbnail__image"
+            src={
+              book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail
+            }
+            alt=""
+          />
+        </div>
+        <div className="bookCard_data__container">
+          <div className="bookCard_title__container">
+            <div className="bookCard_title__text">
+              {book.volumeInfo.title && book.volumeInfo.title}
+            </div>
+            <div className="bookCard_author__text">
+              {book.volumeInfo.authors && book.volumeInfo.authors[0]}
+            </div>
+            <div className="bookCard_ratings__text">
+              {book.volumeInfo.averageRating == null
+                ? 0 + " stars"
+                : book.volumeInfo.averageRating + " stars"}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
--- shop/src/components/main.js
+++ shop/src/components/main.js
@@ -1,5 +1,6 @@
-import react, { useEffect, useReducer, useState } from "react";
-import jaguar from "../assets/jaguar.jpeg";
+import react, { useEffect, useReducer, useState } from "react"
+import jaguar from "../assets/jaguar.jpeg"
+import BookCard from "./BookCard"
 
 // const Secret = () => <h1>This is a secret component</h1>;
 
@@ -16,24 +17,37 @@ import jaguar from "../assets/jaguar.jpeg";
 // const Main = ({ show }) => <>{show ? <Secret /> : <Default />}</>;
 
 const Main = () => {
-  const [checked, setChecked] = useReducer((checked) => !checked, true);
+  const [checked, setChecked] = useReducer((checked) => !checked, true)
   // const [checked, setChecked] = useState(true);
-  const [data, setData] = useState(null);
+  const [data, setData] = useState(null)
 
   useEffect(() => {
-    console.log("%c%s", "color: #00b300", checked);
-  }, [checked]);
+    console.log("%c%s", "color: #00b300", checked)
+  }, [checked])
 
   useEffect(() => {
-    fetch("http://localhost:5289/api/Products")
-      .then((response) => response.json())
-      .then(setData)
-      .catch((err) => console.log(err));
-  }, []);
+    const fetchData = async () => {
+      const subject = "lord of the rings"
+      const totalResults = "40"
+      const data = await fetch(
+        `https://www.googleapis.com/books/v1/volumes?q=${subject}&langRestrict=en&startIndex=0&maxResults=${totalResults}`
+      )
+      const json = await data.json()
+      return json
+    }
+
+    fetchData()
+      .catch(console.error)
+      .then((data) => setData(data.items))
+  }, [])
 
   useEffect(() => {
-    console.log("%c%s", "color: #1d5673", data);
-  }, [data]);
+    console.log("%c%s", "color: #1d5673", data)
+  }, [data])
+
+  function checkData() {
+    console.log(data)
+  }
 
   return (
     <section>
@@ -44,6 +58,13 @@ const Main = () => {
         onChange={setChecked}
       />
 
+      <button onClick={checkData}>Check Data</button>
+
+      <div className="bookDiv">
+        <div className="bookList">
+          {data && data.map((book) => <BookCard key={book.id} book={book} />)}
+        </div>
+      </div>
       {/*
           Create a component which receives
           the list of products and parse into a component 
@@ -53,7 +74,7 @@ const Main = () => {
       <h3>This is an image</h3>
       <img src={jaguar} alt="This is a jaguar" />
     </section>
-  );
-};
+  )
+}
 
-export default Main;
+export default Main
